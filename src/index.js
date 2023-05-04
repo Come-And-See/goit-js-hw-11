200dbd0701203d12099f7a0eee1aa6e232c3c54d
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-05-04 12:38:30 +0300

    UP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import API from './js/fetchPhoto';
 import { Notify } from 'notiflix';
+import axios from 'axios';
 
 import photoCard from './templates/photoCard.hbs';
 
